b7e3cd6496b1d41a0acfe3f9484af8d1aa578dd4
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2025-03-31 19:48:03 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,38 @@
 from django.urls import path
 
-from .views import index
+from . import views
 
 urlpatterns = [
-    path("", index, name="index"),
+    path(
+        "",
+        views.index,
+        name="index"
+    ),
+    path(
+        "manufacturers/",
+        views.ManufacturerListView.as_view(),
+        name="manufacturer-list"
+    ),
+    path(
+        "cars/",
+        views.CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "cars/<int:pk>/",
+        views.CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "drivers/",
+        views.DriverListView.as_view(),
+        name="driver-list"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        views.DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.http import HttpRequest, HttpResponse
 
 from taxi.models import Driver, Car, Manufacturer
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
 
     context = {
@@ -13,3 +15,43 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturers"
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").order_by("id")
+    context_object_name = "cars"
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+    context_object_name = "car"
+
+
+class DriverListView(ListView):
+    model = Driver
+    queryset = Driver.objects.order_by("id")
+    context_object_name = "drivers"
+    template_name = "taxi/driver_list.html"
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "driver"
+
+    def get_queryset(self):
+        return Driver.objects.prefetch_related(
+            "cars__manufacturer"
+        )
